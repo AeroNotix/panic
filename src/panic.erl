652beda98c95b183ff45eacedcceea2f891bafaf
Aaron France <aaron.l.france@gmail.com>
2014-12-13 11:18:33 +0100

    Remove unnecessary `erlang` module specifiers

--- src/panic.erl
+++ src/panic.erl
@@ -7,7 +7,7 @@
 find_big_mboxes(N) ->
     FilterBig =
         fun(Pid) ->
-                case erlang:process_info(Pid, message_queue_len) of
+                case process_info(Pid, message_queue_len) of
                     {_, Size} = All when Size > N ->
                         {true, All};
                     _ ->
@@ -17,7 +17,7 @@ find_big_mboxes(N) ->
     lists:filtermap(FilterBig, processes()).
 
 find_large_memory_consumers(N) ->
-    Pinfo = [{P, recon:info(P, memory)} || P <- erlang:processes()],
+    Pinfo = [{P, recon:info(P, memory)} || P <- processes()],
     Sorter = fun({_, {_, M1}}, {_, {_, M2}}) -> M1 > M2;
                 (_, _) -> false
              end,
