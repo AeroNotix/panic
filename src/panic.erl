3a3184ff4919a6a4d126038fd385588f5bf592f8
Aaron France <aaron.l.france@gmail.com>
2014-12-13 14:17:12 +0100

    Add specs

--- src/panic.erl
+++ src/panic.erl
@@ -5,6 +5,9 @@
 -export([find_large_memory_consumers/1]).
 
 
+-spec
+find_big_mboxes(integer()) ->
+    [{pid(), integer()}].
 find_big_mboxes(N) ->
     FilterBig =
         fun(Pid) ->
@@ -17,13 +20,19 @@ find_big_mboxes(N) ->
         end,
     lists:filtermap(FilterBig, processes()).
 
+-spec
+find_large_memory_consumers(integer()) ->
+    [{pid(), integer()}].
 find_large_memory_consumers(N) ->
     Pinfo = [{P, recon:info(P, memory)} || P <- processes()],
     Sorter = fun({_, {_, M1}}, {_, {_, M2}}) -> M1 > M2;
                 (_, _) -> false
              end,
-    lists:seq(lists:sort(Sorter, Pinfo), N).
+    lists:sublist(lists:sort(Sorter, Pinfo), N).
 
+-spec
+initial_call(pid()) ->
+    atom().
 initial_call(Pid) when is_pid(Pid) ->
     Pinfo = process_info(Pid),
     D = proplists:get_value(dictionary, Pinfo),
